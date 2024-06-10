1bce7295a77ae182af43088016635c3e4375407a
Tiger-Tig74 <75200879+Tiger-Tig74@users.noreply.github.com>
2024-06-10 11:49:13 +0300

    Update my_enumerables.rb

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,64 @@
 module Enumerable
-  # Your code goes here
+  def my_any?
+    for element in self
+      return true if yield(element)
+    end
+    false
+  end
+  def my_all?
+    each { |element| return false unless yield(element)}
+    true
+  end
+  def my_count
+    count = 0
+    return self.length if !block_given?
+    each { |element| count += 1 if yield(element) }
+    count
+  end
+  def my_each_with_index
+    index = 0
+    for element in self
+      yield(element, index)
+      index += 1
+    end
+    self
+  end
+  def my_inject(initial = nil)
+    accumulator = initial.nil? ? first : initial
+    each do |element|
+      accumulator = yield(accumulator, element)
+    end
+    accumulator
+  end
+  def my_map
+    result = []
+    if block_given?
+      each { |element| result << yield(element) }
+    else
+      to_enum(:my_map)
+    end
+    result
+  end
+  def my_none?
+    for element in self
+      return false if yield(element)
+    end
+    true
+  end
+  def my_select
+    result = []
+    if block_given?
+      each { |element| result << element if yield(element) }
+    end
+    result
+  end
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
 class Array
-  # Define my_each here
+  def my_each
+    for element in self
+      yield(element)
+    end
+    self
+  end
 end
